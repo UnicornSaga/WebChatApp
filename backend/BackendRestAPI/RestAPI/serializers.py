9b7c1b31be93f931c81520b67a94666d7fa5e8d2
dothanhdat0907 <75473674+dothanhdat0907@users.noreply.github.com>
2022-04-15 21:43:42 +0700

    Add friend lists

--- backend/BackendRestAPI/RestAPI/serializers.py
+++ backend/BackendRestAPI/RestAPI/serializers.py
@@ -6,6 +6,11 @@ class UserSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=200)
     description = serializers.CharField(max_length=500)
     age = serializers.IntegerField(required=False, default = 18)
+    friendlist = serializers.ListField(
+        serializers.EmailField(max_length=200),
+        max_length=1000,
+        null = True
+    )
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
@@ -15,6 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
         instance.age = validated_data.get('age', instance.age)
+        instance.friendlist = validated_data.get('friendlist', instance.friendlist)
 
         instance.save()
 
