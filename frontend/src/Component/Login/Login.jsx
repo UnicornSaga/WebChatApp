313c56130af96bcb12a3a1bf2e7765146c06b5cd
Vu Minh Vuong <vuongvu1208@gmail.com>
2022-04-21 22:42:03 +0700

    Design Login page

--- frontend/src/Component/Login/Login.jsx
+++ frontend/src/Component/Login/Login.jsx
@@ -2,12 +2,16 @@ import React, { useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./Login.scss";
-import LoginImg from "../../Assets/gojo.jpg";
+import LoginImg1 from "../../Assets/upperImg.png";
+import LoginImg2 from "../../Assets/lowerImg.png";
+import LoginImg3 from "../../Assets/pngwing.png";
+import LoginImg4 from "../../Assets/pngwing1.png";
+import LoginImg5 from "../../Assets/heart.png";
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const { loginWithRedirect, isAuthenticated, logout, user, getAccessTokenSilently } = useAuth0();
+    const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,37 +20,54 @@ const Login = () => {
     }, [isAuthenticated])
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="hideImg">
-                        <img src={LoginImg}/>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className='col-md-6'>
+                        <div className="imgContainer">
+                            <div className='childrenImg1'>
+                                <img src={LoginImg1}/>
+                            </div>
+                            <div className='childrenImg2'>
+                                <img src={LoginImg2}/>
+                            </div>
+                            <div className='childrenImg3'>
+                                <img src={LoginImg5}/>
+                                <img src={LoginImg3}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='col-md-6'>
+                        <div className="formContainer">
+                            <div className="logo">
+                                <img src={LoginImg4} height="60px" width="60px" />
+                            </div>
+                            <h2>Login to your account</h2>
+                            <p>Connect to your friends and family from across the globe!</p>
+                            <button
+                                onClick={(e) => { e.preventDefault(); loginWithRedirect() }}
+                            >
+                                Start Loging In
+                            </button>
+                            {isAuthenticated && <button
+                                onClick={() => logout({
+                                    returnTo: window.location.origin,
+                                })}
+                            >
+                                Logout
+                            </button>}
+                        </div>
                     </div>
                 </div>
 
-                <div className="col-md-6">
-                    <div className="formContainer">
-                        <div className="logo">
-                            <img src={LoginImg} height="60px" width="60px" />
-                        </div>
-                        <h2>Login to your account</h2>
-                        <p>See what is going on in the crypto market</p>
-                        <button
-                            onClick={(e) => { e.preventDefault(); loginWithRedirect() }}
-                        >
-                            Start Loging In
-                        </button>
-                        {isAuthenticated && <button
-                            onClick={() => logout({
-                                returnTo: window.location.origin,
-                            })}
-                        >
-                            Logout
-                        </button>}
+                <footer>
+                    <div>
+                        <p>This projects belongs to Group 6</p>
                     </div>
-                </div>
+                </footer>
             </div>
-        </div>
+        </>
     )
 }
 
