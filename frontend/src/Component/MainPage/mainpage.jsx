04d94d716132bdced6f0010af39aa3566bb4b313
bi0sek <phongtomneptune@gmail.com>
2022-04-26 00:52:53 +0700

    Settings

---
+++ frontend/src/Component/MainPage/mainpage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import io from 'socket.io-client';
+import Messages from '../Messages/Messages';
+import MessageInput from '../MessageInput/MessageInput';
+import { fetchUserInfo } from '../../services/UserInfo.service';
+import { useNavigate } from 'react-router-dom';
+
+const Main = () => {
+    const { logout, getAccessTokenSilently, user } = useAuth0();
+    const { email } = user;
+
+    const [socket, setSocket] = useState(null);
+    const [accessToken, setAccessToken] = useState();
+    const [customer, setCustomer] = useState();
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const newSocket = io(`http://${window.location.hostname}:5000`);
+        setSocket(newSocket);
+        return () => newSocket.close();
+    }, [setSocket]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+              const token = await getAccessTokenSilently();
+              console.log(token);
+              const data = await fetchUserInfo(email, token);
+              console.log(data);
+              setCustomer(data);
+          } catch (e) {
+            console.error(e);
+          }
+        })();
+      }, [getAccessTokenSilently]);
+
+    return (
+        <div>
+            Hello world
+            <button
+                onClick={() => logout({
+                    returnTo: window.location.origin,
+                })}
+            >
+                Logout
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/settings")
+                }}
+            >
+                ⚙ Settings
+            </button>
+
+            <div>
+                { socket ? (
+                    <div>
+                        <Messages socket={socket} />
+                        <MessageInput socket={socket} identity={email} />
+                    </div>
+                ) : (
+                    <div>Not connected</div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Main;
