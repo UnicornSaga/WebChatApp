ba300c3bc22876233e35ed0e30650650fa0088d8
bi0sek <phongtomneptune@gmail.com>
2022-04-26 00:52:53 +0700

    Settings

--- frontend/src/Component/MainPage/mainpage.jsx
+++ frontend/src/Component/MainPage/mainpage.jsx
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 import Messages from '../Messages/Messages';
 import MessageInput from '../MessageInput/MessageInput';
 import { fetchUserInfo } from '../../services/UserInfo.service';
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
     const { logout, getAccessTokenSilently, user } = useAuth0();
@@ -12,6 +13,8 @@ const Main = () => {
     const [socket, setSocket] = useState(null);
     const [accessToken, setAccessToken] = useState();
     const [customer, setCustomer] = useState();
+    
+    const navigate = useNavigate();
 
     useEffect(() => {
         const newSocket = io(`http://${window.location.hostname}:5000`);
@@ -42,6 +45,13 @@ const Main = () => {
                 })}
             >
                 Logout
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/settings")
+                }}
+            >
+                ⚙ Settings
             </button>
 
             <div>
